c904c7efd804b312d103bea01eb5e90e4b2f66ef
Keshav-14 <keshav_14_s@hotmail.com>
2024-03-03 13:41:56 +0530

    parallax javascript
    The script for the parallax effect is coded in this script

---
+++ script.js
@@ -0,0 +1,16 @@
+let text = document.getElementById("text");
+let center_screen = document.getElementById("center-screen");
+let curtain_left = document.getElementById("curtain-left");
+let curtain_right = document.getElementById("curtain-right");
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+
+    if(value <= 275){
+        text.style.marginTop = value * 2.5 + 'px';
+        curtain_left.style.left = value * -.5 + 'px';
+        curtain_right.style.left = value * .5 + 'px';
+    }
+    center_screen.style.top = value * -1 + 'px';
+
+})
