0c530d4ce04285ef88dce558ca1c2e526da00e71
Keshav-14 <keshav_14_s@hotmail.com>
2024-03-22 23:38:29 +0530

    Fixed a bug

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let curtain_right = document.getElementById("curtain-right");
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 + 'px';
-    curtain_left.style.left = value * -.8 + 'px';
+    if(value <= 275){
+        text.style.marginTop = value * 2.5 + 'px';
+    }
+    curtain_left.style.left = value * -.8+ 'px';
     curtain_right.style.left = value * .8 + 'px';
     center_screen.style.top = value * -1 + 'px';
 })
