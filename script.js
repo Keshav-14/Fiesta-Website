1a7b26757e9ea19e06d9053fa7eb10f50b9243ac
Keshav-14 <keshav_14_s@hotmail.com>
2024-03-24 21:56:24 +0530

    The New Changes were Done

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let text = document.getElementById("words");
+let text2 = document.getElementById("scroll-down-div");
 let center_screen = document.getElementById("center-screen");
 let curtain_left = document.getElementById("curtain-left");
 let curtain_right = document.getElementById("curtain-right");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-
+    if(value >= 20) { 
+        text2.style.display = 'none';
+    } 
+    if(value <= 20){
+        text2.style.display = 'flex';
+    }
     if(value <= 275){
         text.style.marginTop = value * 2.5 + 'px';
     }
